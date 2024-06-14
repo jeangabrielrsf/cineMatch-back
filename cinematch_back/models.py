1356bb8dbe9e47a3cd8af7c3897300a9c68b3434
Jean Frusca <jeang96@gmail.com>
2024-06-13 22:42:33 -0300

    created movies table

--- cinematch_back/models.py
+++ cinematch_back/models.py
@@ -12,10 +12,38 @@ class User:
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now(), onupdate=func.now()
     )
-    
+
+    liked_movies: Mapped[list['Movie']] = relationship(
+        init=False, back_populates='user', cascade='all, delete-orphan'
+    )
+
+
+# filmes curtidos pelo usuário
+@table_registry.mapped_as_dataclass
+class Movie:
+    __tablename__ = 'liked_movies'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    overview: Mapped[str]
+    tmdb_id: Mapped[int]
+    popularity: Mapped[float]
+    vote_average: Mapped[float]
+    vote_count: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
+    )
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+
+    user: Mapped[User] = relationship(init=False, back_populates='liked_movies')
